1d2bc24eea624c6d5b92ff0aaa8aa4cfbf87abcc
Rahmad Dawood <rahmad.dawood@unsyiah.ac.id>
2022-03-25 20:59:13 +0700

    Kode untuk digital input.

---
+++ 03-Digital-Input/main.py
@@ -0,0 +1,19 @@
+from machine import Pin
+
+# LED tersambung ke Pin 0.
+led = Pin(0, Pin.OUT)
+# Matikan LED
+led.off()			
+
+# S1 (A1) tersambung ke Pin 2.
+button = Pin(2, Pin.IN)
+
+# Infinite loop
+while True:
+    # Loop selaman S1 ditekan
+    while button.value() == 0:
+        # Hidupkan LED
+        led.on()
+
+    # S1 tidak ditekan matikan LED
+    led.off()	
